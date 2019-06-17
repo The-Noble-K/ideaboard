603f0f00f64413d02fe810f6dc528631bf5e2f9a
Jarod Sneed <jarodsneed@Jarods-MacBook.local>
2019-06-17 13:25:30 -0400

    add/edit ideas

--- src/components/IdeasContainer.js
+++ src/components/IdeasContainer.js
@@ -10,7 +10,8 @@ class IdeasContainer extends Component {
         super(props);
         this.state = {
             ideas: [],
-            editingIdeaId: null
+            editingIdeaId: null,
+            notification: ''
         }
     }
 
@@ -46,6 +47,26 @@ class IdeasContainer extends Component {
         .catch(error => console.log(error))
     }
 
+    updateIdea = (idea) => {
+        const ideaIndex = this.state.ideas.findIndex(x => x.id === idea.id)
+        const ideas = update(this.state.ideas, {
+            [ideaIndex]: { $set: idea }
+        })
+        this.setState({
+            ideas: ideas,
+            notification: 'All changes saved'
+        })
+    }
+
+    resetNotification = () => {
+        this.setState({notification: ''})
+    }
+
+    enableEditing = (id) => {
+        this.setState({editingIdeaId: id},
+            () => {this.title.focus()})
+    }
+
     render() {
         return(
             <div>
@@ -53,12 +74,18 @@ class IdeasContainer extends Component {
                     <button className="newIdeaButton" onClick={this.addNewIdea}>
                         New Idea
                     </button>
+                    <span className="notification">
+                        {this.state.notification}
+                    </span>
                 </div>
                 {this.state.ideas.map((idea) => {
                     if(this.state.editingIdeaId === idea.id) {
-                        return(<IdeaForm idea={idea} key={idea.id} />)
+                        return(<IdeaForm idea={idea} key={idea.id} 
+                            updateIdea={this.updateIdea}
+                            titleRef={input => this.title = input}
+                            resetNotification={this.resetNotification} />)
                     } else {
-                        return(<Idea idea={idea} key={idea.id} />)
+                        return(<Idea idea={idea} key={idea.id} onClick={this.enableEditing}/>)
                     }
                 })}
             </div>  
